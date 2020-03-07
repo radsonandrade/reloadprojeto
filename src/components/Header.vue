fc6b90719f2e4f185ffd0350b5a53a34223f377b
Radson <radsonandrade@gmail.com>
2020-03-06 22:10:48 -0300

    Add components Header and Footer

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="header">
+        <header class="header">
+            <div class="img-header">
+                <img src="" alt="">
+            </div>
+            <div class="nav-header">
+                <nav class="nav-header">
+                    <div class="router-links">
+                        <router-link to ="/">Página Inicial</router-link>
+                        <router-link to ="/about">Sobre Nós</router-link>
+                        <router-link to ="/contact">Contatos</router-link>
+                        
+                    </div>
+                </nav>
+            </div>
+
+        </header>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+header {
+    width: 100vw;
+    height: 80px;
+
+}
+
+</style>
